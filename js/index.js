28033375fad01f1bd768df83038df6c181e05a6c
shokhruh61 <shohruxmahmudov61@gmail.com>
2024-10-03 18:00:19 +0500

    bug:js

---
+++ js/index.js
@@ -0,0 +1,208 @@
+//masala 1
+// let son1 = +prompt("Butun son kiriting:");
+// if (son1 > 0) {
+//   son1 += 1;
+// }
+// console.log(son1);
+//masala 2
+// let son2 = +prompt("Butun son kiriting:");
+// if (son2 > 0) {
+//   son2 -= 2;
+// }
+// console.log(son2);
+//masala 3
+// let son3 = +prompt("Butun son kiriting:");
+// if (son3 > 0) {
+//   son3 += 1;
+// } else if (son3 < 0) {
+//   son3 -= 2;
+// } else {
+//   son3 = 10;
+// }
+// console.log(son3);
+//masala 4
+// let son4_1 = +prompt("1-sonni kiriting:");
+// let son4_2 = +prompt("2-sonni kiriting:");
+// let son4_3 = +prompt("3-sonni kiriting:");
+
+// let musbatSoni = 0;
+// if (son4_1 > 0) musbatSoni++;
+// if (son4_2 > 0) musbatSoni++;
+// if (son4_3 > 0) musbatSoni++;
+
+// console.log("Musbat sonlar soni:", musbatSoni);
+//masala 5
+// let son5_1 = +prompt("1-sonni kiriting:");
+// let son5_2 = +prompt("2-sonni kiriting:");
+// let son5_3 = +prompt("3-sonni kiriting:");
+
+// let musbatSoni = 0;
+// let manfiySoni = 0;
+
+// if (son5_1 > 0) musbatSoni++;
+// else manfiySoni++;
+// if (son5_2 > 0) musbatSoni++;
+// else manfiySoni++;
+// if (son5_3 > 0) musbatSoni++;
+// else manfiySoni++;
+
+// console.log("Musbat sonlar soni:", musbatSoni);
+// console.log("Manfiy sonlar soni:", manfiySoni);
+//masala 6
+// let son6_1 = +prompt("1-sonni kiriting:");
+// let son6_2 = +prompt("2-sonni kiriting:");
+
+// let kattasi = Math.max(son6_1, son6_2);
+// console.log("Katta son:", kattasi);
+//masala 7
+// let son7_1 = +prompt("1-sonni kiriting:");
+// let son7_2 = +prompt("2-sonni kiriting:");
+
+// let kichigi = Math.min(son7_1, son7_2);
+// console.log("Kichik son:", kichigi);
+//masala 8
+// let son8_1 = +prompt("1-sonni kiriting:");
+// let son8_2 = +prompt("2-sonni kiriting:");
+
+// let kattasi8 = Math.max(son8_1, son8_2);
+// let kichigi8 = Math.min(son8_1, son8_2);
+
+// console.log("Katta son:", kattasi8);
+// console.log("Kichik son:", kichigi8);
+//masala 9
+// let son9_1 = +prompt("1-sonni kiriting:");
+// let son9_2 = +prompt("2-sonni kiriting:");
+
+// if (son9_1 !== son9_2) {
+//   if (son9_1 > son9_2) {
+//     son9_2 = son9_1;
+//   } else {
+//     son9_1 = son9_2;
+//   }
+// } else {
+//   son9_1 = son9_2 = 0;
+// }
+
+// console.log("A:", son9_1);
+// console.log("B:", son9_2);
+// let son10_1 = +prompt("1-sonni kiriting:");
+// let son10_2 = +prompt("2-sonni kiriting:");
+//masala 10
+// if (son10_1 !== son10_2) {
+//   if (son10_1 > son10_2) {
+//     son10_2 = son10_1;
+//   } else {
+//     son10_1 = son10_2;
+//   }
+// } else {
+//   son10_1 = son10_2 = 0;
+// }
+
+// console.log("A:", son10_1);
+// console.log("B:", son10_2);
+//masala 11
+// let son11_1 = +prompt("1-sonni kiriting:");
+// let son11_2 = +prompt("2-sonni kiriting:");
+
+// if (son11_1 !== son11_2) {
+//   if (son11_1 > son11_2) {
+//     son11_2 = son11_1;
+//   } else {
+//     son11_1 = son11_2;
+//   }
+// } else {
+//   son11_1 = son11_2 = 0;
+// }
+
+// console.log("A:", son11_1);
+// console.log("B:", son11_2);
+//masala 12
+// let son12_1 = +prompt("1-sonni kiriting:");
+// let son12_2 = +prompt("2-sonni kiriting:");
+
+// let kichikSon = Math.min(son12_1, son12_2);
+// console.log("Kichik son:", kichikSon);
+//masala 13
+// let son13_1 = +prompt("1-sonni kiriting:");
+// let son13_2 = +prompt("2-sonni kiriting:");
+// let son13_3 = +prompt("3-sonni kiriting:");
+
+// let ortachaSon =
+//   son13_1 +
+//   son13_2 +
+//   son13_3 -
+//   Math.max(son13_1, son13_2, son13_3) -
+//   Math.min(son13_1, son13_2, son13_3);
+// console.log("O'rtacha son:", ortachaSon);
+//masala 14
+// let son14_1 = +prompt("1-sonni kiriting:");
+// let son14_2 = +prompt("2-sonni kiriting:");
+
+// let kattasi14 = Math.max(son14_1, son14_2);
+// let kichigi14 = Math.min(son14_1, son14_2);
+
+// console.log("Katta son:", kattasi14);
+// console.log("Kichik son:", kichigi14);
+//masala 15
+// let son15_1 = +prompt("1-sonni kiriting:");
+// let son15_2 = +prompt("2-sonni kiriting:");
+// let son15_3 = +prompt("3-sonni kiriting:");
+
+// let yigindisi = son15_1 + son15_2 + son15_3;
+// let kattasi15 = Math.max(son15_1, son15_2, son15_3);
+// let natija = yigindisi - kattasi15;
+
+// console.log("Natija:", natija);
+
+//masala 16
+// let A1 = +prompt("A sonni kiriting:");
+// console.log(A1 > 0 ? "A soni musbat" : "A soni musbat emas");
+//masala 17
+// let A2 = +prompt("A sonni kiriting:");
+// console.log(A2 % 2 !== 0 ? "A soni toq" : "A soni juft");
+//masala 18
+// let A3 = +prompt("A sonni kiriting:");
+// console.log(A3 % 2 === 0 ? "A soni juft" : "A soni toq");
+//masala 19
+// let A4 = +prompt("A sonni kiriting:");
+// let B4 = +prompt("B sonni kiriting:");
+// console.log(A4 ** 2 > B4 && B4 <= 3 ? "A^2 va B<=3" : "Shart bajarilmadi");
+//masala 20
+// let A5 = +prompt("A sonni kiriting:");
+// let B5 = +prompt("B sonni kiriting:");
+// console.log(A5 >= 0 || B5 < -2 ? "A >= 0 yoki B < -2" : "Shart bajarilmadi");
+//masala 21
+// let A6 = +prompt("A sonni kiriting:");
+// let B6 = +prompt("B sonni kiriting:");
+// let C6 = +prompt("C sonni kiriting:");
+// console.log(A6 <= B6 && B6 <= C6 ? "A <= B <= C" : "Shart bajarilmadi");
+//masala 22
+// let A8 = +prompt("A sonni kiriting:");
+// let B8 = +prompt("B sonni kiriting:");
+// console.log(A8 % 2 !== 0 && B8 % 2 !== 0 ? "A va B sonlari toq" : "A va B sonlari toq emas");
+//masala 23
+// let A9 = +prompt("A sonni kiriting:");
+// let B9 = +prompt("B sonni kiriting:");
+// console.log(A9 % 2 === 0 && B9 % 2 === 0 ? "A va B sonlarining hech biri toq emas" : "Shart bajarilmadi");
+//masala 24
+// let A10 = +prompt("A sonni kiriting:");
+// let B10 = +prompt("B sonni kiriting:");
+// console.log((A10 % 2 !== 0 && B10 % 2 === 0) || (A10 % 2 === 0 && B10 % 2 !== 0) ? "A va B sonlaridan faqat bittasi toq" : "Shart bajarilmadi");
+//masala 25
+// const dollarNarxi = 11000.34; // 1 AQSH dollari
+// const yevroNarxi = 12354.03; // 1 yevro
+// const samolyotBileti = 500 * dollarNarxi;
+// const mehmonxonaTolovi = 250 * dollarNarxi;
+// const muzeyVaKongilocharJoyi = 120 * yevroNarxi;
+
+// let AlisherdaPul = +prompt("Alisherda qancha pul borligini kiriting (so'mda):");
+
+// const jamiXarajatlar = samolyotBileti + mehmonxonaTolovi + muzeyVaKongilocharJoyi;
+
+// // Xarajatlarni hisoblash
+// if (AlisherdaPul >= jamiXarajatlar) {
+//     console.log("Oq yo'l, Alisher!");
+// } else {
+//     console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.");
+// }
+
